4d65bea065767826f179c811fb14f9de83b5dec6
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-08-17 12:49:05 +0530

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=>{
     try {
-        // await mongoose.connect(process.env.MONGO_URI)
-        await mongoose.connect('mongodb://localhost:27017/ecommerce')
+        await mongoose.connect(process.env.MONGO_URI)
+        // await mongoose.connect('mongodb://localhost:27017/ecommerce')
     console.log('MongoDB Connected')
     } catch (error) {
      console.log(error)   
