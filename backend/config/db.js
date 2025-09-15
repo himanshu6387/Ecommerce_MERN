67aba51721a97e63c72cb1784ca6e8e3d995b786
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-09-15 16:52:48 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect('mongodb+srv://abhiranjanms5646_db_user:<db_password>@cluster1.pdqjeh9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1')
+        await mongoose.connect(process.env.MONGO_URI)
         // await mongoose.connect('mongodb://localhost:27017/ecommerce')
     console.log('MongoDB Connected')
     } catch (error) {
