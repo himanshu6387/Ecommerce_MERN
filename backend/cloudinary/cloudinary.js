d00c22d8de3534d6ca742ee0c5518fd02eceda7f
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-09-15 17:44:45 +0530

    fine

--- backend/cloudinary/cloudinary.js
+++ backend/cloudinary/cloudinary.js
@@ -1,9 +1,13 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-    cloud_name:'dsgtxyq0c',
-    api_key:'283611941129434',
-    api_secret:'YelV0_9cRSXhfIx5fVyKMSOvsI8'
+    cloud_name:'dingg2tq9',
+    api_key:'976634486382574',
+    api_secret:'BEQ3q2x3R8fqUPaRDhpsnDUW11k'
+
+    // cloud_name:'dsgtxyq0c',
+    // api_key:'283611941129434',
+    // api_secret:'YelV0_9cRSXhfIx5fVyKMSOvsI8'
 })
 
 module.exports = cloudinary
