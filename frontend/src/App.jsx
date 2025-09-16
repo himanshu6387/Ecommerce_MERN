c2646172d3ab0d7e731573279c1aa54f6546ca06
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-09-16 14:55:01 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Signup from './pages/Signup';
 import AdminDashboard from './pages/Admindashboard';
 import UserDashboard from './pages/UserDashboard';
 import Cart from './pages/Cart';
-import { AuthProvider } from './context/AuthContext';
+import { AuthContext, AuthProvider } from './context/AuthContext';
 import CreateProduct from './pages/CreateProduct';
 import GetAllProducts from './pages/GetAllProduct';
 import Home from './pages/Home';
@@ -19,8 +19,13 @@ import PopularCategories from './pages/PopularCategories';
 import Wishlist from './pages/Wishlist';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
+import { useContext } from 'react';
+import ProtectedRoute from './components/ProtectedRoute';
+
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <AuthProvider>
       <BrowserRouter>
@@ -29,16 +34,50 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/admin/create-product" element={<CreateProduct />} />
-          <Route path="/admin/products" element={<GetAllProducts />} />
-          <Route path="/shop" element={<UserDashboard />} />
-          <Route path="/cart" element={<Cart />} />
+
+          {/* ✅ Only logged-in users can access */}
+          <Route path="/admin" element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/admin/create-product" element={
+            <ProtectedRoute>
+              <CreateProduct />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/admin/products" element={
+            <ProtectedRoute>
+              <GetAllProducts />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/shop" element={
+            <ProtectedRoute>
+              <UserDashboard />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/cart" element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          } />
+
+          <Route path="/wishlist" element={
+            <ProtectedRoute>
+              <Wishlist />
+            </ProtectedRoute>
+          } />
+
+          {/* Public routes */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/carousel" element={<Carousel />} />
           <Route path="/categories" element={<PopularCategories />} />
-          <Route path="/wishlist" element={<Wishlist />} />
+
           {/* Forgot + Reset Password */}
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
@@ -46,7 +85,7 @@ function App() {
         <Toaster />
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
